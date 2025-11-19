dde24a1b99562b709722dfd6c51e33d6ffe57c0a
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2025-11-20 00:53:41 +0100

    afficher les detail des travailleur +ajouter icons pour le suppression

--- workspace.js
+++ workspace.js
@@ -1,5 +1,191 @@
 const worker=document.getElementById('worker');
 const addworkerBtn=document.getElementById('addWorkerBtn');
+
+
+const UnassignedWorker = document.getElementById('UnassignedWorker');
+const descWorker = document.getElementById('descWorker');
+const WorkerData = [
+
+     {
+        name: "Ahmed",
+        image:"images/WhatsApp Image 2025-11-13 à 22.38.16_da362682.jpg",
+        role: "Réceptionniste",
+        company: "Tech Solutions SARL", 
+
+        experiences: [
+            {
+                Company:"Z12",
+                position: "Support IT",
+                duration: "Jan 2022 - Dec 2022",
+                description: "Maintenance des systèmes et assistance aux utilisateurs."
+            },
+            {
+                Company:"Z12",
+                position: "Technicien réseau",
+                duration: "Jan 2023 - Present",
+                description: "Gestion des infrastructures réseau et sécurité informatique."
+            }
+        ]
+    },
+
+    {
+        
+        name: "khalid",
+        image:"https://t4.ftcdn.net/jpg/04/31/64/75/360_F_431647519_usrbQ8Z983hTYe8zgA7t1XVc5fEtqcpa.jpg",
+        role: "Techniciens IT",
+        company: "Tech Solutions SARL", 
+
+        experiences: [
+            {
+                Company:"Z12",
+                position: "Support IT",
+                duration: "Jan 2022 - Dec 2022",
+                description: "Maintenance des systèmes et assistance aux utilisateurs."
+            },
+            {
+                Company:"Z12",
+                position: "Technicien réseau",
+                duration: "Jan 2023 - Present",
+                description: "Gestion des infrastructures réseau et sécurité informatique."
+            }
+        ]
+    },
+
+ 
+    {
+        name: "Fatima",
+        image: "https://blog.photofeeler.com/wp-content/uploads/2018/01/sample-linkedin-headshot-good.jpg",
+        role: "Manager",
+        company: "Tech Solutions SARL", 
+
+        experiences: [
+            {
+                  Company:"Z12",
+                position: "Support IT",
+                duration: "Jan 2022 - Dec 2022",
+                description: "Maintenance des systèmes et assistance aux utilisateurs."
+            },
+            {
+                  Company:"Z12",
+                position: "Technicien réseau",
+                duration: "Jan 2023 - Present",
+                description: "Gestion des infrastructures réseau et sécurité informatique."
+            }
+        ]
+    },
+     {
+        name: "mohamed",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrJaxYg64cSX1flsMAcGFwZ071HCZq94kO2igLp4OBtRrmjoWDVM1Zd7tfcBYLFaejLe4&usqp=CAU",
+        role: "Nettoyage",
+        company: "Tech Solutions SARL", 
+
+        experiences: [
+            {
+                Company:"Z12",
+                post: "Support IT",
+                duration: "Jan 2022 - Dec 2022",
+                description: "Maintenance des systèmes et assistance aux utilisateurs."
+            },
+            {
+                company: "Tech Solutions SARL",
+                post: "Technicien réseau",
+                duration: "Jan 2023 - Present",
+                description: "Gestion des infrastructures réseau et sécurité informatique."
+            }
+        ]
+    },
+    {
+        
+        name: "Aicha",
+        image: "https://media.gettyimages.com/id/1437816897/fr/photo/portrait-de-femme-daffaires-de-gestionnaire-ou-de-ressources-humaines-pour-la-r%C3%A9ussite.jpg?s=612x612&w=gi&k=20&c=tfcvEVTcJcfXTtA0rB-NbjurEVpp7N3QN9heh7Q0RuU=",
+        role: "sécurité",
+        company: "Tech Solutions SARL", 
+
+        experiences: [
+            {
+                Company:"Z12",
+                post: "Manager",
+                duration: "Jan 2022 - Dec 2021",
+                description: "Maintenance des systèmes et assistance aux utilisateurs."
+            },
+            {
+                company: "Tech Solutions SARL",
+                position: "Technicien réseau",
+                duration: "Feb 2025 - Present",
+                description: "Gestion des infrastructures réseau et sécurité informatique."
+            }
+        ]
+    }
+];
+
+const UnassignedWorkerData=[];
+
+WorkerData.forEach((e, index) => {
+
+    const div = document.createElement('div');
+    div.classList.add('worker-card');  
+    div.dataset.index = index;
+
+    div.innerHTML = `
+        <img src="images/icons8-close-24.png" class="img2">
+        <img src="${e.image}">
+        <ul>
+            <li>${e.name}</li>
+            <li>${e.role}</li>
+        </ul>
+        <button class="edit">Edit</button>
+    `;
+   
+    UnassignedWorker.appendChild(div);
+
+    const worker2 = {
+        name: e.name,
+        role: e.role,
+        image:e.image
+    };
+   UnassignedWorkerData.push(worker2);
+});
+
+
+function afficherDetail(worker) {
+    descWorker.innerHTML = `
+        <img src="images/icons8-close-24.png" class="deleteicon">
+        <img src="${worker.image}" class="imgDescrp">
+        <ul>
+            <li><strong>Name:</strong> ${worker.name}</li>
+            <li><strong>Role:</strong> ${worker.role}</li>
+            <li><strong>Company:</strong> ${worker.company}</li>
+        </ul>
+
+        <strong><h3>Experiences:</h3></strong>
+        <ul>
+            ${worker.experiences
+                .map(exp => `
+                    <li>
+                        <strong>${exp.position || exp.post}</strong>  
+                        (${exp.duration})  
+                        <br> ${exp.description}
+                    </li>
+                `)
+                .join("")
+            }
+        </ul>
+    `;
+}
+document.querySelectorAll('.worker-card').forEach(card => {
+    card.addEventListener('click', () => {
+        const index = card.dataset.index;  
+        afficherDetail(WorkerData[index]); 
+    });
+});
+
+
+descWorker.addEventListener('click', (e) => {
+    if (e.target.classList.contains('deleteicon')) {
+        descWorker.innerHTML = "";  
+    }
+});
+
 addworkerBtn.addEventListener('click',(e)=>{
        e.preventDefault();
     worker.innerHTML="";
@@ -65,6 +251,7 @@ addworkerBtn.addEventListener('click',(e)=>{
     </div>
 </form>
     `; 
+   
 const input = document.getElementById('photo');
 const preview = document.getElementById('preview');
 
@@ -80,10 +267,10 @@ const  addExperienceBtn=document.getElementById('addExperienceBtn');
 const  experiences=document.getElementById('experiences');
 addExperienceBtn.addEventListener('click',()=>{
     experiences.innerHTML+=`
-    <div class="flex flex-col p-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-violet-400 bg-gray-100">
-     <label class="font-semibold text-gray-700">Company</label>
-     <input type="text" class="p-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-violet-400 outline-none text-black " ></input>
-     <label class="font-semibold text-gray-700">Role</label>
+    <div class="experiences flex flex-col p-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-violet-400 bg-gray-100">
+     <label class=" font-semibold text-gray-700">Company</label>
+     <input type="text" class="company p-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-violet-400 outline-none text-black " ></input>
+     <label class=" font-semibold text-gray-700">Role</label>
      <input type="text" class="p-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-violet-400 outline-none text-black " ></input>
      <label class="font-semibold text-gray-700">From</label>
     <input type="date"   class="p-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-violet-400 outline-none text-black">
@@ -94,11 +281,14 @@ addExperienceBtn.addEventListener('click',()=>{
 })
 const saveBtn=document.getElementById('saveBtn');
 const closeBtn=document.getElementById('closeBtn');
+
+
 saveBtn.addEventListener('click',(e)=>{
     e.preventDefault();
 const name=document.getElementById('name');
 const role = document.getElementById('role');
 const preview = document.getElementById('preview');
+const experiences = document.getElementById('experiences');
 const div=document.createElement('div');
 div.classList.add('worker-card');
 div.innerHTML=`
@@ -114,7 +304,17 @@ div.innerHTML=`
 
 `;
    UnassignedWorker.appendChild(div);
-   
+   const worker0={
+    name:name.value,
+    image:preview.src,
+    role:role.value,
+    experiences:{
+        company:experiences.firstChild.value,
+        position:experiences.lastChild.value,
+    }
+
+   }
+UnassignedWorkerData.push(worker0);
 
 }); 
 
@@ -124,63 +324,7 @@ closeBtn.addEventListener('click',()=>{
 });
 
 });
-
-const UnassignedWorker = document.getElementById('UnassignedWorker');
-const WorkerData = [
-
-     {
-        name: "Ahmed",
-        image:"images/WhatsApp Image 2025-11-13 à 22.38.16_e5b4bc95.jpg",
-        role: "Réceptionniste"
-    },
-
-    {
-        name: "khalid",
-        image:"https://t4.ftcdn.net/jpg/04/31/64/75/360_F_431647519_usrbQ8Z983hTYe8zgA7t1XVc5fEtqcpa.jpg",
-         role: "Techniciens IT"
-    },
-
-    {
-        name: "Fatima",
-        image: "https://blog.photofeeler.com/wp-content/uploads/2018/01/sample-linkedin-headshot-good.jpg",
-        role: "Manager",
-    },
-     {
-        name: "mohamed",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrJaxYg64cSX1flsMAcGFwZ071HCZq94kO2igLp4OBtRrmjoWDVM1Zd7tfcBYLFaejLe4&usqp=CAU",
-        role: "Nettoyage",
-    },
-    {
-        name: "Aicha",
-        image: "https://media.gettyimages.com/id/1437816897/fr/photo/portrait-de-femme-daffaires-de-gestionnaire-ou-de-ressources-humaines-pour-la-r%C3%A9ussite.jpg?s=612x612&w=gi&k=20&c=tfcvEVTcJcfXTtA0rB-NbjurEVpp7N3QN9heh7Q0RuU=",
-        role: "sécurité",
-    }
-];
-
-const UnassignedWorkerData=[];//data of unassignedWorker
-WorkerData.forEach(e=>{
-
-    const div=document.createElement('div');
-
-    div.classList.add('worker-card');
-
-    div.innerHTML=`
-        <img src="${e.image}">
-        <ul>
-          <li>${e.name}</li>
-          <li>${e.role}</li>
-        </ul>
-     <button class="edit">Edit</button>
-    `;
-    UnassignedWorker.appendChild(div);
-    const worker2 = {
-        name: e.name,
-        role: e.role,
-        image:e.image
-    };
-   UnassignedWorkerData.push(worker2);
-});
-
+    
 
 //fonction return les travailleur par role
 function getparRole(workers, role1) {
@@ -203,7 +347,6 @@ const zoneAddBtn = document.querySelectorAll('.zoneAddBtn');
             const div = document.createElement('div');
             div.classList.add('workersvaliables');
             div.innerHTML = `
-               <img src="images/icons8-close-24.png" class="img2">
                <img id="img1" src="${worker.image}" >
                <div>
                <ul>
@@ -260,7 +403,7 @@ fetch('data.json')
 
 document.addEventListener('click', (e) => {
     if (e.target.classList.contains('img2')) {
-        const workerDiv = e.target.closest('.workersvaliables');
+        const workerDiv = e.target.closest('.worker-card ');
         if (workerDiv) {
             workerDiv.remove();
         }
@@ -268,6 +411,22 @@ document.addEventListener('click', (e) => {
 });
 
 
+// const UnassignedWorker=document.getElementById('UnassignedWorker');
+//   const descWorker=document.getElementById('descWorker');
+//  cards.forEach(element=>{
+
+//   if()
+//       element.addEventListener('click',()=>{
+//          descWorker.innerHTML="";
+//          descWorker.innerHTML=`
+//      <div class="block text-ms  flex">
+//         <ul>
+//          <li><p>Nam:${element.name}</p></li>
+//           <li><p>Role:${element.role}</p></li>
+//           </ul>       
+//     </div>
+//   `;
+//       })
+//   })
 
  
-
